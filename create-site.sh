9a3f2c35525870a81b02a13a73af82c410917446
Ionut Staicu <github@iamntz.com>
2017-02-23 19:21:44 +0200

    added named args to specify additional data

--- create-site.sh
+++ create-site.sh
@@ -12,53 +12,87 @@
 # groupdel user
 #
 #
-# apt install sendmail imagemagick nginx php-fpm php-mysql
-# https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-in-ubuntu-16-04
+# apt install sendmail imagemagick nginx php-fpm php-mysql php-dom letsencrypt
 #
+# echo "AllowGroups sshusers" >> /etc/ssh/sshd_config
+# addgroup sshusers && adduser root sshusers
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
+# https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-in-ubuntu-16-04
+
+
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in
+    -d=*|--domain=*)
+    DOMAIN="${i#*=}"
+    shift
+    ;;
+    -u=*|--user=*)
+    USER="${i#*=}"
+    shift
+    ;;
+    --wp_password=*)
+    WP_PASSWORD="${i#*=}"
+    shift
+    ;;
+    --wp_mail=*)
+    WP_MAIL="${i#*=}"
+    shift
+    ;;
+    --wp_admin=*)
+    WP_ADMIN="${i#*=}"
+    shift
+    ;;
+    *)
+        DOMAIN=$2
+        USER=$1
+    ;;
+esac
+done
+
 
 service nginx stop
 
 # creating default folder structure
-mkdir -p /var/www/$2/htdocs
-mkdir -p /var/www/$2/logs
-touch /var/www/$2/logs/access.log
-touch /var/www/$2/logs/error.log
-touch /var/www/$2/htdocs/index.php
+mkdir -p /var/www/${DOMAIN}/htdocs
+mkdir -p /var/www/${DOMAIN}/logs
+touch /var/www/${DOMAIN}/logs/access.log
+touch /var/www/${DOMAIN}/logs/error.log
+touch /var/www/${DOMAIN}/htdocs/index.php
 
 # adding group for new site
-groupadd $1
+groupadd ${USER}
 # adding user for the new site
-useradd -g $1 $1 -d /var/www/$2 -G sshusers
+useradd -g ${USER} ${USER} -d /var/www/${DOMAIN} -G sshusers
 # maybe add to www-data as well?
-#useradd -g $1 $1 -d /var/www/$2 -G www-data
+#useradd -g ${USER} ${USER} -d /var/www/${DOMAIN} -G www-data
 
 # changing permissions for the new site
-chown -R $1:$1 /var/www/$2
+chown -R ${USER}:${USER} /var/www/${DOMAIN}
 
 # copying  nginx config
-cp ./templates/nginx.conf /etc/nginx/sites-available/$2
+cp ./templates/nginx.conf /etc/nginx/sites-available/${DOMAIN}
 
 #copying pool config to limit access to an user
-cp ./templates/pool.conf /etc/php/7.0/fpm/pool.d/$1.conf
+cp ./templates/pool.conf /etc/php/7.0/fpm/pool.d/${USER}.conf
 
 # replacing dummy value with the real domain name
-sed -i s/__SITE_NAME__/$2/g /etc/nginx/sites-available/$2
-sed -i s/__USER_NAME__/$1/g /etc/nginx/sites-available/$2
-ln -s /etc/nginx/sites-available/$2 /etc/nginx/sites-enabled/$2
+sed -i s/__SITE_NAME__/${DOMAIN}/g /etc/nginx/sites-available/${DOMAIN}
+sed -i s/__USER_NAME__/${USER}/g /etc/nginx/sites-available/${DOMAIN}
+ln -s /etc/nginx/sites-available/${DOMAIN} /etc/nginx/sites-enabled/${DOMAIN}
 
 
 # replacing dummy value with real user name
-sed -i s/__SITE_NAME__/$1/g /etc/php/7.0/fpm/pool.d/$1.conf
+sed -i s/__SITE_NAME__/${USER}/g /etc/php/7.0/fpm/pool.d/${USER}.conf
 
 # generating let's encrypt certificate
-# /root/letsencrypt/certbot-auto certonly --standalone -d $2
-letsencrypt certonly --standalone -d $2
-
+letsencrypt certonly --standalone -d ${DOMAIN}
 
 # restarting PHP & nginx
 service php7.0-fpm restart
 service nginx start
-
 
 # create random password
 PASSWDDB="$(openssl rand -base64 12)"
@@ -68,26 +102,42 @@ MAINDB=${USER_NAME//[^a-zA-Z0-9]/_}
 
 # If /root/.my.cnf exists then it won't ask for root password
 if [ -f /root/.my.cnf ]; then
-    mysql -e "CREATE DATABASE $1 /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER $1@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -e "GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';"
+    mysql -e "CREATE DATABASE ${USER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${USER}.* TO '${USER}'@'localhost';"
     mysql -e "FLUSH PRIVILEGES;"
 # If /root/.my.cnf doesn't exist then it'll ask for root password
 else
     echo "Please enter root user MySQL password!"
     read rootpasswd
-    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE $1 /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -uroot -p${rootpasswd} -e "CREATE USER $1@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';"
+    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${USER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -uroot -p${rootpasswd} -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${USER}.* TO '${USER}'@'localhost';"
     mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 fi
 
-echo "Domain: $2"
-echo "Mysql user: $1"
+
+sudo -u ${USER} -i -- wp core download --path=htdocs
+sudo -u ${USER} -i -- wp core config --path=htdocs --dbname=${USER} --dbuser=${USER} --dbpass=${PASSWDDB}
+
+if [[ -z $WP_PASSWORD ]]; then
+    $WP_PASSWORD = "$(openssl rand -base64 6)"
+fi
+
+if [[ -z $WP_MAIL ]]; then
+    $WP_MAIL = admin@${DOMAIN}
+fi
+
+if [[ -z $WP_ADMIN ]]; then
+    $WP_ADMIN = admin
+fi
+
+sudo -u ${USER} -i -- wp core install --path=htdocs --url=https://${DOMAIN} --title="${DOMAIN}" --admin_user=${WP_ADMIN} --admin_password=${WP_PASSWORD} --admin_email=${WP_MAIL}
+sudo -H -u ${USER} bash -c 'mkdir ~/.ssh && ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C "dev@dev" && cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys'
+
+echo "Domain: ${DOMAIN}"
+echo "Mysql user: ${USER}"
 echo "Mysql Password: ${PASSWDDB}"
 
-sudo -u $1 -i -- wp core download --path=htdocs
-sudo -u $1 -i -- wp core config --path=htdocs --dbname=$1 --dbuser=$1 --dbpass=${PASSWDDB}
-sudo -u $1 -i -- wp core install --path=htdocs --url=https://$2 --title="$2" --admin_user=admin --admin_password=123 --admin_email=admin@$2
-
-sudo -H -u $1 bash -c 'mkdir ~/.ssh && ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C "dev@dev" && cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys'
+echo "wp-admin user: ${WP_ADMIN}"
+echo "wp-admin password: ${WP_PASSWORD}"

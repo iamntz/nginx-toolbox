8c21bac671208bee9ecdfadab99c245c2565ea8e
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2017-04-11 12:10:23 +0545

    some changes

--- create-site.sh
+++ create-site.sh
@@ -12,71 +12,28 @@
 # groupdel user
 #
 #
-# sudo apt-get install sendmail imagemagick nginx php-fpm php-mysql php-dom letsencrypt composer phpunit mariadb-server-10.0 mariadb-client-core-10.0 php-curl php-gd
+# apt install sendmail imagemagick nginx php-fpm php-mysql php-dom letsencrypt composer phpunit mariadb-server-10.0 mariadb-client-core-10.0 php-gd php-curl
 #
-# sudo echo "AllowGroups sshusers" >> /etc/ssh/sshd_config
-# sudo addgroup sshusers && adduser root sshusers
+# echo "AllowGroups sshusers" >> /etc/ssh/sshd_config
+# addgroup sshusers && adduser root sshusers
 # curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 
-# sudo echo "cgi.fix_pathinfo=0" >> /etc/php/7.0/fpm/php.ini
+# echo "cgi.fix_pathinfo=0" >> /etc/php/7.0/fpm/php.ini
 # https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-in-ubuntu-16-04#configure-the-php-processor
 
 
+# add
+# client_max_body_size 100m;
+# on /etc/nginx/nginx.conf  (http block)
+# 
+# 
+# add
+# upload_max_filesize = 100M
+# post_max_size = 100M
+# on /etc/php/7.0/fpm/php.ini
 
-show_help()
-{
-    echo "
-Quick Usage:
-    You just need to specify the user name that will run the site and
-    the fully qualified domain name (i.e. without the http part; subdomains also works)
-
-    e.g. ./create-site.sh site_user my.site.com
-
-Advanced usage:
-    -d, --domain        FQ domain
-    -u, --user          site_user
-    --wp_password       wordpress-password
-    --wp_mail           wordpress-admin-mail
-    --wp_admin          wordpress-admin-user
-
--h, --help              Print this help"
-    exit 1
-}
-
-
-for i in "$@"
-do
-case $i in
-    -h | --help )
-    show_help
-    exit 1
-    ;;
-    -d=*|--domain=*)
-    DOMAIN="${i#*=}"
-    shift
-    ;;
-    -u=*|--user=*)
-    USER="${i#*=}"
-    shift
-    ;;
-    --wp_password=*)
-    WP_PASSWORD="${i#*=}"
-    shift
-    ;;
-    --wp_mail=*)
-    WP_MAIL="${i#*=}"
-    shift
-    ;;
-    --wp_admin=*)
-    WP_ADMIN="${i#*=}"
-    shift
-    ;;
-    *)
-        DOMAIN=$2
-        USER=$1
-    ;;
-esac
-done
+DOMAIN=$2
+USER=$1
 
 service nginx stop
 
@@ -98,10 +55,12 @@ useradd -g $USER $USER -d /var/www/$DOMAIN -G sshusers
 chown -R $USER:$USER /var/www/$DOMAIN
 
 # copying  nginx config
-cp templates/nginx.conf /etc/nginx/sites-available/$DOMAIN
+cp -i  templates/nginx.conf /etc/nginx/sites-available/$DOMAIN
 
 #copying pool config to limit access to an user
-cp templates/pool.conf /etc/php/7.0/fpm/pool.d/$USER.conf
+cp -i templates/pool.conf /etc/php/7.0/fpm/pool.d/$USER.conf
+
+# cp -i templates/robots.txt /var/www/${DOMAIN}/htdocs/robots.txt
 
 # replacing dummy value with the real domain name
 sed -i s/__SITE_NAME__/$DOMAIN/g /etc/nginx/sites-available/$DOMAIN
@@ -119,51 +78,55 @@ letsencrypt certonly --standalone -d $DOMAIN
 service php7.0-fpm restart
 service nginx start
 
-# create random password
-PASSWDDB="$(openssl rand -base64 12)"
-
-# replace "-" with "_" for database username
-MAINDB=${USER_NAME//[^a-zA-Z0-9]/_}
-
-# If /root/.my.cnf exists then it won't ask for root password
-if [ -f /root/.my.cnf ]; then
-    mysql -e "CREATE DATABASE ${USER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${USER}.* TO '${USER}'@'localhost';"
-    mysql -e "FLUSH PRIVILEGES;"
-# If /root/.my.cnf doesn't exist then it'll ask for root password
-else
-    echo "Please enter root user MySQL password!"
-    read rootpasswd
-    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${USER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -uroot -p${rootpasswd} -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';"
-    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${USER}.* TO '${USER}'@'localhost';"
-    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-fi
-
+# If Install WP else just create subdomain
 read -p "Install WP (Y/n)? " -n 1 -r
 echo ""
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+    PASSWDDB="$(openssl rand -base64 12)"
+
+    # replace "-" with "_" for database username
+    MAINDB=${USER_NAME//[^a-zA-Z0-9]/_}
+    # If /root/.my.cnf exists then it won't ask for root password
+    if [ -f /root/.my.cnf ]; then
+        mysql -e "CREATE DATABASE ${USER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+        mysql -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+        mysql -e "GRANT ALL PRIVILEGES ON ${USER}.* TO '${USER}'@'localhost';"
+        mysql -e "FLUSH PRIVILEGES;"
+    # If /root/.my.cnf doesn't exist then it'll ask for root password
+    else
+        echo -n "MySql Root Password: "
+        read -s rootpasswd
+        mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${USER} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+        mysql -uroot -p${rootpasswd} -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';"
+        mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${USER}.* TO '${USER}'@'localhost';"
+        mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+    fi
     sudo -u $USER -i -- wp core download --path=htdocs
     sudo -u $USER -i -- wp core config --path=htdocs --dbname=$USER --dbuser=$USER --dbpass=${PASSWDDB}
 
+    if [[ -z $WP_ADMIN ]]; then
+        echo -n "WP Admin Username: "
+        read WP_ADMIN
+    fi    
     if [[ -z $WP_PASSWORD ]]; then
-        WP_PASSWORD="$(openssl rand -base64 6)"
+        echo -n "WP Admin Password: "
+        read WP_PASSWORD
     fi
 
     if [[ -z $WP_MAIL ]]; then
-        WP_MAIL=admin@$DOMAIN
+        echo -n "WP Admin Email: "
+        read WP_MAIL
     fi
-
-    if [[ -z $WP_ADMIN ]]; then
-        WP_ADMIN=admin
-    fi
-
     echo "Installing WordPress. This may take a while."
     sudo -u $USER -i -- wp core install --path=htdocs --url=https://$DOMAIN --title="${DOMAIN}" --admin_user=${WP_ADMIN} --admin_password=${WP_PASSWORD} --admin_email=${WP_MAIL}
+fi
+
+if [ ! -f "/var/www/${DOMAIN}/.ssh/id_rsa" ]; then
     sudo -H -u $USER bash -c 'mkdir ~/.ssh && ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C "dev@dev" && cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys'
 fi
+
+cat ~/.ssh/authorized_keys  >> "/var/www/${DOMAIN}/.ssh/authorized_keys"
 
 echo "Domain: ${DOMAIN}"
 echo "Mysql user: ${USER}"
